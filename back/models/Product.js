74f3e31e2bdf4a7da686e21ec31b8e740bc1b36f
luc <luc.bourrat.42@gmail.com>
2021-09-24 14:41:43 +0200

    Commit P5 with EN trad

---
+++ back/models/Product.js
@@ -0,0 +1,90 @@
+const products = [
+  {
+    "colors": ["Blue", "White", "Black"],
+    "id": "107fb5b75607497b96722bda5b504926",
+    "name": "Kanap Sinopé",
+    "price": 1849,
+    "imageUrl": "kanap01.jpeg",
+    "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "altTxt": "Photo of a blue sofa, two seats"
+  },
+  {
+    "colors": ["Black/Yellow", "Black/Red"],
+    "id": "415b7cacb65d43b2b5c1ff70f3393ad1",
+    "name": "Kanap Cyllène",
+    "price": 4499,
+    "imageUrl": "kanap02.jpeg",
+    "description": "Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.",
+    "altTxt": "Photo of a yellow and black sofa, four seats"
+  },
+  {
+    "colors": ["Green", "Red", "Orange"],
+    "id": "055743915a544fde83cfdfc904935ee7",
+    "name": "Kanap Calycé",
+    "price": 3199,
+    "imageUrl": "kanap03.jpeg",
+    "description": "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
+    "altTxt": "Photo of a green angle sofa, three seats"
+  },
+  {
+    "colors": ["Pink", "White"],
+    "id": "a557292fe5814ea2b15c6ef4bd73ed83",
+    "name": "Kanap Autonoé",
+    "price": 1499,
+    "imageUrl": "kanap04.jpeg",
+    "description": "Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.",
+    "altTxt": "Photo of a pink sofa, one to two seats"
+  },
+  {
+    "colors": ["Grey", "Purple", "Blue"],
+    "id": "8906dfda133f4c20a9d0e34f18adcf06",
+    "name": "Kanap Eurydomé",
+    "price": 2249,
+    "imageUrl": "kanap05.jpeg",
+    "description": "Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.",
+    "altTxt": "Photo of a gray sofa, three seats"
+  },
+  {
+    "colors": ["Grey", "Navy"],
+    "id": "77711f0e466b4ddf953f677d30b0efc9",
+    "name": "Kanap Hélicé",
+    "price": 999,
+    "imageUrl": "kanap06.jpeg",
+    "description": "Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.",
+    "altTxt": "Photo of a gray sofa, two seats"
+  },
+  {
+    "colors": ["Red", "Silver"],
+    "id": "034707184e8e4eefb46400b5a3774b5f",
+    "name": "Kanap Thyoné",
+    "price": 1999,
+    "imageUrl": "kanap07.jpeg",
+    "description": "EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.",
+    "altTxt": "Photo of a red sofa, two seats"
+  },
+  {
+    "colors": ["Pink", "Brown", "Yellow", "White"],
+    "id": "a6ec5b49bd164d7fbe10f37b6363f9fb",
+    "name": "Kanap orthosie",
+    "price": 3999,
+    "imageUrl": "kanap08.jpeg",
+    "description": "Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.",
+    "altTxt": "Photo of a pink sofa, three seats"
+  }
+];
+
+exports.find = () => {
+  return new Promise((resolve, reject) => resolve(JSON.parse(JSON.stringify(products))));
+}
+
+exports.findById = (id) => {
+  return new Promise((resolve, reject) =>
+    resolve(JSON.parse(JSON.stringify(products)).find(product =>
+      product._id == id)
+    )
+  );
+}
+
+
+
+
